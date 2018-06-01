ed7f736cc4bdf02fc43a5fa22edc4596292f82c3
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 17:19:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+def my_collect(array)
+    i = 0 
+    new_array = []
+    while i < array.length
+      new_array << yield(array[i]) 
+      i += 1
+    end
+  new_array
+end
 
+# def my_each(array)
+#   i = 0 
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+#   array
+# end
 
+# def hello_t(array)
+#   i = 0
+#   while i < array.length
+#     yield array[i]
+#     i = i + 1
+#   end
+# end
