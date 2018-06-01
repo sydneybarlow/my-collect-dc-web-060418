f1709d78a4e0f35e0ae9a9bee3b80fcdd622e62e
Sydney Barlow <sydneybarlow44@gmail.com>
2018-06-01 17:02:05 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+def my_collect(languages)
+    i = 0 
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+    array
+  end
+end
 
+# def my_each(array)
+#   i = 0 
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+#   array
+# end
 
+# def hello_t(array)
+#   i = 0
+#   while i < array.length
+#     yield array[i]
+#     i = i + 1
+#   end
+# end
